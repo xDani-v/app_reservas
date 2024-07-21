f5e8b529c582902a9737da615a6815c89797c7aa
xDani-v <villanodaniel8@gmail.com>
2024-07-21 10:12:51 -0500

    ventana login, recuperacion y inicio

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,30 @@
 import { Routes } from '@angular/router';
+import { LoginComponent } from './components/login/login.component';
+import { PrincipalComponent } from './components/principal/principal.component';
+import { RecuperarComponent } from './components/recuperar/recuperar.component';
+import { RegistroComponent } from './components/registro/registro.component';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+    {
+        path: 'login',
+        component: LoginComponent
+    },
+    {
+        path: 'recuperar',
+        component: RecuperarComponent
+    },
+    {
+        path: 'registro',
+        component: RegistroComponent
+    },
+    {
+        path: 'home',
+        component: PrincipalComponent,
+
+    },
+    {
+        path: '**',
+        component: LoginComponent
+    },
+
+];
